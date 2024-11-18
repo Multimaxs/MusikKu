12114473ad80eca7d24af92471fe449c23e72108
Multimaxs <jordiabrar@gmail.com>
2024-11-18 08:27:07 +0700

    tambah page

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/artis', function () {
+    return view('artis');
+});
+
+Route::get('/genre', function () {
+    return view('genre');
+});
+
+Route::get('/mood', function () {
+    return view('mood');
+});
