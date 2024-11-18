76de17e294420b79a3b72c420ab89bae138e94d6
Multimaxs <jordiabrar@gmail.com>
2024-11-18 08:00:54 +0700

    upload

---
+++ resources/views/home.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('assets/CSS/style.css') }}">
+</head>
+<body>
+    <div class="profile-card">
+        <div class="profile-picture">
+            <img src="c33d83d9618e4c82a17d7a14278f6316.jpg" alt="Profile Picture">
+        </div>
+        <div class="profile-content">
+            <h1><span class="typing-animation">My Profile</span></h1>
+            <p>Hai! Nama saya JORDI ABRAR SATRIA. Saya adalah mahasiswa Teknik Informatika dengan minat besar pada pengembangan perangkat lunak dan kecerdasan buatan. Saat ini, saya sedang mendalami pemrograman dengan bahasa seperti Python, Java, dan C++, serta membangun proyek-proyek menggunakan framework seperti React dan Django. Saya juga tertarik pada bidang data science, machine learning, dan pengembangan aplikasi mobile. Saya percaya bahwa teknologi dapat menjadi solusi untuk berbagai tantangan global, dan saya bercita-cita untuk berkontribusi dalam inovasi teknologi di masa depan.</p>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+            <div>
+                <h2>Socials</h2>
+                <a href="#"><i class="fab fa-linkedin"></i></a>
+                <a href="#"><i class="fab fa-github"></i></a>
+                <a href="#"><i class="fab fa-facebook"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+                <a href="#"><i class="fab fa-youtube"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+            </div>
+        </div>
+    </div>
+        <div class="gallery-container">
+            <h1>Gallery</h1>
+        </div>
+        <div class="gallery-grid">
+            <img src="c2fa17ead20c29a60d07220df8d0cd09.jpg" alt="Image 1">
+            <img src="33d95ee7324687c137bbe39ada23d106.jpg" alt="Image 2">
+            <img src="590a0a6bd6ca955ed8afca24dc336970.jpg" alt="Image 3">
+            <img src="a95c503df3f4775e7c32d6dcf814e248.jpg" alt="Image 4">
+            <img src="f014ad6432a6aad6eae8559dab356fff.jpg" alt="Image 5">
+            <img src="7823f0b4f181d10344139be65ac0cd7a.jpg" alt="Image 6">
+            <img src="d66b865ee761a0ae94008cf7f026721a.jpg" alt="Image 7">
+            <img src="06181a08c1498f0f88a511bf67569445.jpg" alt="Image 8">
+        </div>
+    </div>
+</body>
+</html>
