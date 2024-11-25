009b8cb267de6ed9068358a1779b132db727f767
unknown <frshalaisya@gmail.com>
2024-11-26 02:13:20 +0700

    update

--- resources/views/genre.blade.php
+++ resources/views/genre.blade.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>List Mood</title>
-    <link rel="stylesheet" href="{{ asset('assets/CSS/artis.css') }}">
+    <title>List Genre</title>
+    <link rel="stylesheet" href="{{ asset('assets/CSS/genre.css') }}">
 </head>
 <body>
-    <section class="mood-list">
+    <section class="genre-list">
         <div class="search-bar">
-            <input type="text" placeholder="Cari Mood" />
+            <input type="text" placeholder="Cari Genre" />
         </div>
         <div class="container">
             @if ($genre->isEmpty())
@@ -17,14 +17,14 @@
                     Data tidak ditemukan
                 </div>
             @else
-                <div class="artis-grid">
+                <div class="genre-grid">
                     @foreach ($genre as $section)
-                        <div class="artis-card">
+                        <div class="genre-card">
                             <a href="{{ url('/genre/' . $section->tipe_id) }}">
-                                <div class="artis-image">
+                                <div class="genre-image">
                                     <img loading="lazy" src="{{ Storage::url($section->thumbnail) }}" alt="{{ $section->title }}">
                                 </div>
-                                <div class="artis-name">
+                                <div class="genre-name">
                                     <p>{{ $section->title }}</p>
                                 </div>
                             </a>
--- resources/views/mood.blade.php
+++ resources/views/mood.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>List Mood</title>
-    <link rel="stylesheet" href="{{ asset('assets/CSS/artis.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/CSS/mood.css') }}">
 </head>
 <body>
     <section class="mood-list">
