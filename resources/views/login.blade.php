0baceb5a69549e2dffedbe995ed46e2822061e73
inayahdanis <inayahkhofifahdanis@gmail.com>
2024-11-30 09:39:20 +0700

    update

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -8,6 +8,8 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
     <!-- Tambahkan CSS Kustom -->
     <link rel="stylesheet" href="{{ asset('assets/CSS/akun.css') }}">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Jockey+One&display=swap">
+
 </head>
 <body>
 <div class="container">
